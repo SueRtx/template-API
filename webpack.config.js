ea3bbce09a4a01678bec27ed765d99c4091e1c5f
Sue Roberts <suerobertstx@yahoo.com>
2022-05-29 18:57:15 -0500

    Update template

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
-/*module.exports = {
+module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'eval-source-map',
-  devServer: {               
-    contentBase: './dist'    
+  devServer: {
+    contentBase: './dist'
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'template',
+      title: 'Planetary Age',
       template: './src/index.html',
       inject: 'body'
     }),
@@ -25,17 +25,36 @@ const Dotenv = require('dotenv-webpack');
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /.css$/,
         use: [
           'style-loader',
           'css-loader'
         ]
       },
       {
-        test: /\.js$/,
+        test: /.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
       }
     ]
   }
-};*/
+};
